82b83d332b948f5353c20c3974417246512cbf6d
Alexandru Furculita <alex@rhetina.com>
2016-04-13 06:07:10 +0300

    Remove builder assets and icons

--- public/builder/extensions/ribbon/js/ribbonimage_init.js
+++
@@ -1,8 +0,0 @@
-jQuery('.ribbon-image').each(function() {
-	var _float = jQuery(this).data('float');
-	var _zindex = jQuery(this).data('zindex');
-	jQuery(this).fluidbox({
-		// stackIndex: _zindex
-		stackIndex: 1000
-	});
-});
